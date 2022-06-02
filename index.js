607c34c401bcf906af82368f3b09d310bfa6f954
Jerrorld <jerrorldk@gmail.com>
2022-06-01 20:25:42 -0700

    recreated code using doc create elements

--- index.js
+++ index.js
@@ -13,10 +13,35 @@ content.append(dogContent)
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
 content.append(dogDetails)
 
+let descr = document.createElement('h3')
+descr.textContent = 'Description:'
+dogDetails.append(descr)
 
+let par = document.createElement('p')
+par.textContent = 'This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.'
+dogDetails.append(par)
+
+let feeding = document.createElement('h3')
+feeding.textContent = 'Feeding Times:'
+dogDetails.append(feeding)
+
+let list = document.createElement('ul')
+let list1 = document.createElement('li')
+let list2 = document.createElement('li')
+let list3 = document.createElement('li')
+list1.textContent = "9:00 am"
+list2.textContent = "12:00 pm"
+list3.textContent = "5:00 pm"
+
+list.append(list1)
+list.append(list2)
+list.append(list3)
+dogDetails.append(list)
+
+dogContent.append(dogDetails)
